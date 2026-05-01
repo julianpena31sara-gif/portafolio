bc88ccf1347fc47df3a06f9ec26d7bdb6957d2f0
julianpena31sara-gif <julianpena31sara@gmail.com>
2026-05-01 17:56:05 -0500

    portafolio

---
+++ portfolio/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  const ext = path.extname(filePath);
+  const contentTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.ico': 'image/x-icon'
+  };
+
+  const contentType = contentTypes[ext] || 'text/html';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err2, data2) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data2);
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Portfolio corriendo en puerto ${PORT}`);
+});
